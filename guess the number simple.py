5663fcc2e7c5ab860e3a73fdd935c46bcdfb2d0a
ohem-source <ohem4444@gmail.com>
2026-02-01 02:05:31 +0200

    Update guess the number simple.py

--- guess the number simple.py
+++ guess the number simple.py
@@ -3,7 +3,7 @@ import random
 def guess_the_number():
     random_number = random.randint(1, 100)
     max_attemps = 7
-    
+    won = False    
     print("Welcome to 'Guess The Number', it's very important to sit down and relax before using this amazing program. Please choose an integer between 1 to 100. Be careful, you have only seven tries. Good luck son.")
     
     for attemp in range(1, max_attemps + 1):
@@ -11,14 +11,17 @@ def guess_the_number():
         user_input = int(input())
         
         if user_input == random_number:
+            won = True
             print('Congrats son. You must be special')
-            return  # Exit the function immediately when won
+            break 
         elif user_input < random_number:
-            print('The number you chose is lower than the actual number')
+            print('Too low!')
         else:
-            print("Too high, be calm and try again")
+            print("Too high!")
     
-    # This only runs if the loop completes without returning
-    print(f"\nSon, you're not ready yet. The number you were supposed to guess was {random_number}")
+    
+    if not won: 
+            print(f"\nSon, you're not ready yet. The number you were supposed to guess was {random_number}")
+
 
 guess_the_number()
