bfbb3919c6d4ec97b62abfa51ec45e6cdce30f33
ohem-source <ohem4444@gmail.com>
2026-02-01 02:22:48 +0200

    guess the number simple.py

--- guess the number simple.py
+++ guess the number simple.py
@@ -4,7 +4,7 @@ def guess_the_number():
     random_number = random.randint(1, 100)
     max_attempts = 7
     #Addition variable: I've added a boolean variable to check if the user won, to avoid showing the final message when the user didn't guess correctly after seven tries.
-    hasWon = False    
+    has_won = False    
     print("Welcome to 'Guess The Number', it's very important to sit down and relax before using this amazing program. Please choose an integer between 1 to 100. Be careful, you have only seven tries. Good luck son.")
     
     for attempt in range(1, max_attempts + 1):
@@ -12,7 +12,7 @@ def guess_the_number():
         user_input = int(input())
         
         if user_input == random_number:
-            hasWon = True
+            has_won = True
             print('Congrats son. You must be special')
             break 
         elif user_input < random_number:
@@ -21,10 +21,11 @@ def guess_the_number():
             print("Too high!")
     
     
-    if not hasWon: 
+    if not has_won: 
             print(f"\nSon, you're not ready yet. The number you were supposed to guess was {random_number}")
 
 
 guess_the_number()
+
 
 
