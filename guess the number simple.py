3b0a22c730ae6f77f89262d30296032b2737aae4
ohem-source <ohem4444@gmail.com>
2026-02-01 02:09:37 +0200

    guess the number simple.py

--- guess the number simple.py
+++ guess the number simple.py
@@ -2,12 +2,12 @@ import random
 
 def guess_the_number():
     random_number = random.randint(1, 100)
-    max_attemps = 7
+    max_attempts = 7
     won = False    
     print("Welcome to 'Guess The Number', it's very important to sit down and relax before using this amazing program. Please choose an integer between 1 to 100. Be careful, you have only seven tries. Good luck son.")
     
-    for attemp in range(1, max_attemps + 1):
-        print(f"Attempt {attemp}:")
+    for attempt in range(1, max_attempts + 1):
+        print(f"Attempt {attempt}:")
         user_input = int(input())
         
         if user_input == random_number:
@@ -15,7 +15,7 @@ def guess_the_number():
             print('Congrats son. You must be special')
             break 
         elif user_input < random_number:
-            print('Too low!')
+            print("Too low!")
         else:
             print("Too high!")
     
@@ -25,3 +25,4 @@ def guess_the_number():
 
 
 guess_the_number()
+
